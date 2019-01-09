d4d4f8dd745b5323cf612771e9ae030532a8ad60
Davit Khurshudyan <davo11122@gmail.com>
2019-01-09 10:26:11 +0400

    Minimal webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const config = {
+    entry: './src/index.ts',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 9000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.svg$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.tsx',
+            '.ts',
+            '.js'
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: require('html-webpack-template'),
+            inject: false,
+            appMountId: 'app',
+        })
+    ],
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\\/]node_modules[\\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    }
+}
+
+module.exports = config;
