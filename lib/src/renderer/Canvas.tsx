4486c2413d11cd62ccce8cd8b4f352842cecc230
Friedrich Lindenberg <friedrich@pudo.org>
2019-05-03 16:39:29 +0200

    Remove dragArea helper method

--- lib/src/renderer/Canvas.tsx
+++ lib/src/renderer/Canvas.tsx
@@ -62,14 +62,10 @@ export class Canvas extends React.Component <ICanvasProps> {
     }
   }
 
-  private getDragArea() {
-    return Rectangle.fromPoints(this.dragInitial, this.dragExtent)
-  }
-
   private resizeSelection() {
     const selection = this.selectionRef.current
     if (selection) {
-      const rect = this.getDragArea()
+      const rect = Rectangle.fromPoints(this.dragInitial, this.dragExtent)
       selection.setAttribute('x', rect.x + '')
       selection.setAttribute('y', rect.y + '')
       selection.setAttribute('width', rect.width + '')
@@ -147,7 +143,6 @@ export class Canvas extends React.Component <ICanvasProps> {
     const viewport = this.nextViewport || this.props.viewport
     const grid = `M ${viewport.gridUnit} 0 L 0 0 0 ${viewport.gridUnit}`
     const style = {width: "100%", height: "100%"}
-    const rect = this.getDragArea()
     return (
       <svg viewBox={viewport.viewBox} style={style} ref={this.svgRef} xmlns="http://www.w3.org/2000/svg" className="canvas">
         <DraggableCore
@@ -165,10 +160,6 @@ export class Canvas extends React.Component <ICanvasProps> {
             {this.props.children}
             <rect id="selection"
                   ref={this.selectionRef}
-                  x={rect.x}
-                  y={rect.y}
-                  width={rect.width}
-                  height={rect.height}
                   stroke="black"
                   strokeWidth="0.5px"
                   strokeDasharray="2"
