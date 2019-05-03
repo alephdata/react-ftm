63a829e89030838d51422e4819b9a16f1e6f2ede
Friedrich Lindenberg <friedrich@pudo.org>
2019-05-03 06:46:36 +0200

    accidentally committed the wrong grapheditor

--- lib/src/GraphEditor.tsx
+++ lib/src/GraphEditor.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
-import { GraphLayout, GraphUpdateHandler } from './layout'
+import { Button, ButtonGroup } from '@blueprintjs/core';
+import { GraphLayout, GraphUpdateHandler } from './layout/GraphLayout'
 import { GraphRenderer } from './renderer/GraphRenderer'
-import { Point } from './layout/Point';
+import { Toolbar } from './Toolbar';
 
 export interface IGraphEditorProps {
   layout: GraphLayout,
@@ -12,42 +13,33 @@ export class GraphEditor extends React.Component<IGraphEditorProps> {
   constructor(props: any) {
     super(props)
     this.onZoom = this.onZoom.bind(this)
-    this.onToggleSelectionMode = this.onToggleSelectionMode.bind(this)
   }
 
   onZoom(factor: number) {
-    const { layout, updateLayout } = this.props
-    const { viewport } = layout
+    const {layout, updateLayout} = this.props
+    const {viewport} = layout
     const newZoomLevel = viewport.zoomLevel * factor
-    const newCenter = new Point(
-      viewport.center.x * -1 * newZoomLevel,
-      viewport.center.x * -1 * newZoomLevel
-    )
-    layout.viewport = viewport.setZoom(newCenter, newZoomLevel)
-    updateLayout(layout)
-  }
-
-  onToggleSelectionMode() {
-    const { layout, updateLayout } = this.props
-    layout.selectionMode = !layout.selectionMode
+    layout.viewport = viewport.setZoom(viewport.center, newZoomLevel)
     updateLayout(layout)
   }
 
   render() {
-    const { layout, updateLayout } = this.props;
+    const {layout, updateLayout} = this.props;
     return (
-      <React.Fragment>
-        <div>
-          <button onClick={this.onToggleSelectionMode}>s-mode: {layout.selectionMode + ''}</button>
+      <div style={{flex: 1, display: 'flex', flexFlow: 'column', height: '100%'}}>
+        <div style={{flexGrow: 0, flexShrink: 1, flexBasis: 'auto'}}>
+          <Toolbar layout={layout} updateLayout={updateLayout}/>
         </div>
-        <div style={{borderWidth: 1, borderColor: '#000', borderStyle: 'solid', position: 'relative', width: '100%'}}>
-          <div style={{position: 'absolute', top: '2em', right: '2em'}}>
-            <button onClick={() => this.onZoom(0.8)}>+</button>
-            <button onClick={() => this.onZoom(1.2)}>-</button>
+        <div style={{flexGrow: 1, flexShrink: 1, flexBasis: '100%', position: 'relative'}}>
+          <div style={{position: 'absolute', top: '10px', right: '10px'}}>
+            <ButtonGroup minimal={true} vertical>
+              <Button icon="zoom-in" onClick={() => this.onZoom(0.8)}/>
+              <Button icon="zoom-out" onClick={() => this.onZoom(1.2)}/>
+            </ButtonGroup>
           </div>
-          <GraphRenderer layout={layout} updateLayout={updateLayout} />
+          <GraphRenderer layout={layout} updateLayout={updateLayout}/>
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
