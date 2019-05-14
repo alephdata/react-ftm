486121d6a98b025ca1c4f0158c2b34d8451f4d91
Davit Khurshudyan <davo11122@gmail.com>
2019-05-14 14:32:32 +0200

    resverting readonly props form config

--- lib/src/GraphConfig.ts
+++ lib/src/GraphConfig.ts
@@ -3,12 +3,12 @@ import { Point } from './layout/Point';
 
 export class GraphConfig {
   public gridUnit: number = 10
-  public readonly VERTEX_COLOR: string = Colors.BLUE5
-  public readonly SELECTED_COLOR: string = Colors.GOLD4
-  public readonly SELECTED_COLOR2: string = Colors.GRAY4
-  public readonly VERTEX_RADIUS: number = 1
-  public readonly contentPadding: string = '10px'
-  public readonly BORDER_COLOR: string = Colors.LIGHT_GRAY1
+  public VERTEX_COLOR: string = Colors.BLUE5
+  public SELECTED_COLOR: string = Colors.GOLD4
+  public SELECTED_COLOR2: string = Colors.GRAY4
+  public VERTEX_RADIUS: number = 1
+  public contentPadding: string = '10px'
+  public BORDER_COLOR: string = Colors.LIGHT_GRAY1
 
   gridToPixel(point: Point): Point {
     return new Point(
