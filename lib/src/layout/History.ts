dc8f772a061fe00dc0687828e4dccb02e4fb04cf
Davit Khurshudyan <davo11122@gmail.com>
2019-06-07 14:29:30 +0400

    improved history

--- lib/src/layout/History.ts
+++ lib/src/layout/History.ts
@@ -4,36 +4,36 @@ import {GraphLayout, IGraphLayoutData} from "./GraphLayout";
 export class History {
   static BACK = -1;
   static FORWARD = 1;
-  isPooling:boolean = false;
-  stack:Array<IGraphLayoutData> = [];
-  current?:IGraphLayoutData
-  state:number = 0;
+  isPooling: boolean = false;
+  stack: Array<IGraphLayoutData> = [];
+  current?: IGraphLayoutData
+  state: number = 0;
   private layout: GraphLayout;
 
-  constructor(layout:GraphLayout) {
+  constructor(layout: GraphLayout) {
     this.layout = layout
-    this.push(layout.toJSON())
   }
-
-   push(item:IGraphLayoutData){
-    if(this.isPooling) return undefined;
-    this.stack.splice(this.state+1, this.stack.length, item);
+  push(item: IGraphLayoutData) {
+    if (this.isPooling) return undefined;
+    this.stack.splice(this.state + 1, this.stack.length, item);
     this.state = this.stack.length - 1;
     this.current = this.stack[this.state]
   }
-  createChangePool(modifier:Function){
+
+  createChangePool(modifier: Function) {
     this.isPooling = true;
     modifier();
     this.isPooling = false;
     this.push(this.layout.toJSON())
   }
-  go(factor:number):GraphLayout{
+
+  go(factor: number): GraphLayout {
     const nextPossibleState = this.state += factor;
     let nextState;
 
-    if(factor > 0){
+    if (factor > 0) {
       nextState = Math.min(nextPossibleState, this.stack.length - 1)
-    }else {
+    } else {
       nextState = Math.max(nextPossibleState, 0)
     }
 
@@ -41,17 +41,24 @@ export class History {
     this.current = this.stack[this.state];
     return this.layout.update(this.current)
   }
-  canGoTo(factor:number):boolean{
+
+  canGoTo(factor: number): boolean {
     debugger;
     const nextPossibleState = this.state + factor;
-    if(!factor){
+    if (!factor) {
       return true
-    }else if(factor > 0){
+    } else if (factor > 0) {
       return nextPossibleState <= (this.stack.length - 1)
-    }else {
+    } else {
       return nextPossibleState >= 0
     }
   }
-  back(){ return this.go(History.BACK) }
-  forward(){  return this.go(History.FORWARD) }
+
+  back() {
+    return this.go(History.BACK)
+  }
+
+  forward() {
+    return this.go(History.FORWARD)
+  }
 }
