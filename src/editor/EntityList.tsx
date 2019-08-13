c4f4074f1e9967329b5b0e2f822dd21b1ef73679
kjacks <kjack227@gmail.com>
2019-08-13 11:57:02 +0200

    styled entitylsit component

--- src/editor/EntityList.tsx
+++ src/editor/EntityList.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Entity } from '@alephdata/followthemoney';
 import { Menu, MenuItem } from '@blueprintjs/core'
 import { SchemaIcon } from '../types';
+import './EntityList.scss';
+
 
 interface IEntityListProps {
   entities: Array<Entity>
@@ -13,9 +15,10 @@ export class EntityList extends React.PureComponent<IEntityListProps>{
     () => this.props.onEntitySelected && this.props.onEntitySelected(entity)
   render(){
     const {entities} = this.props;
-    return <Menu>
+    return <Menu className="EntityList">
       {entities.map((entity) =>
         <MenuItem
+          className="EntityList__item"
           onClick={this.onClickFactory(entity)}
           key={entity.id}
           text={entity.getCaption()}
