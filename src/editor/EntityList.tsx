07e0d2f3703d48f7c650fb6e9d29e8009633d4a6
kjacks <kjack227@gmail.com>
2019-08-16 13:19:57 +0200

    added entity list categories

--- src/editor/EntityList.tsx
+++ src/editor/EntityList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Entity } from '@alephdata/followthemoney';
 import { Menu, MenuItem } from '@blueprintjs/core'
 import { SchemaIcon } from '../types';
+import { groupBy } from '../utils'
 import './EntityList.scss';
 
 
@@ -15,16 +16,26 @@ export class EntityList extends React.PureComponent<IEntityListProps>{
     () => this.props.onEntitySelected && this.props.onEntitySelected(entity)
   render(){
     const {entities} = this.props;
+    const entityGroups = groupBy(entities, (e:Entity) => e.schema)
     return <Menu className="EntityList">
-      {entities.map((entity) =>
-        <MenuItem
-          className="EntityList__item"
-          onClick={this.onClickFactory(entity)}
-          key={entity.id}
-          text={entity.getCaption()}
-          icon={SchemaIcon.get(entity.schema)}
-        />
-      )}
+      {Object.entries(entityGroups).map(([key, values]:any) => {
+        return (
+          <div className="EntityList__category" key={key}>
+            <h5 className="EntityList__category__title">{key}</h5>
+            <div className="EntityList__category__values">
+              {values.map((entity: Entity ) =>
+                <MenuItem
+                  className="EntityList__item"
+                  onClick={this.onClickFactory(entity)}
+                  key={entity.id}
+                  text={entity.getCaption()}
+                  icon={SchemaIcon.get(entity.schema)}
+                />
+              )}
+            </div>
+          </div>
+        )
+      })}
     </Menu>
   }
 }
