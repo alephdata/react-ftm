adf2e22a103ad163e9565cc053546a19b5e8cf00
kjacks <kjack227@gmail.com>
2019-11-08 12:54:22 +0100

    added blueprint css files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/select/lib/css/blueprint-select.css';
+import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
+import '@blueprintjs/table/lib/css/table.css';
+import './index.scss';
+
 export * from './layout'
 export * from './editor'
 export * from './renderer'
