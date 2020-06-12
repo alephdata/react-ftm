bf7c8091ae082e3dc36d1b78c5b1c7487f8912ef
kjacks <kjack227@gmail.com>
2020-06-12 13:14:14 +0200

    fine tuned horizontal and vertical scales

--- src/layout/tools/alignHorizontal.ts
+++ src/layout/tools/alignHorizontal.ts
@@ -1,4 +1,5 @@
-import { forceLink, forceManyBody, forceSimulation, forceY } from "d3-force";
+import { forceLink, forceManyBody, forceSimulation, forceY, forceCollide } from "d3-force";
+import { scaleLinear } from 'd3-scale';
 import { Point, Vertex } from "../";
 import { IPositioningProps } from './common';
 import getForceData from './getForceData';
@@ -8,9 +9,11 @@ const alignHorizontal = (props:IPositioningProps): any => {
   const { center, nodes, links, groupingLinks } = getForceData(props);
 
   const simulation = forceSimulation(nodes)
+    .force("collide", forceCollide().radius(5).strength(.01))
+    .force('links', forceLink(links).strength(.03))
     .force("y", forceY(center.y).strength(10))
-    .force('groupingLinks', forceLink(groupingLinks).strength(1).distance(3))
-    .force("charge", forceManyBody().strength(-1))
+    .force('groupingLinks', forceLink(groupingLinks).strength(2).distance(3))
+    .force("charge", forceManyBody().strength(-2))
     .stop()
     .tick(300)
 
@@ -21,13 +24,19 @@ const alignHorizontal = (props:IPositioningProps): any => {
     }
   };
 
+  const yOffsetScale = scaleLinear()
+    .domain([1, 100])
+    .range([2, 15]);
+
   const positionEdge = (e:Edge, i:number) => {
     const source = nodes.find(n => n.id === e.sourceId);
     const target = nodes.find(n => n.id === e.targetId);
     if (source && target) {
       const x = (source.x + target.x)/2;
-      const y = i%2 ? center.y + 5 : center.y - 5;
-      return new Point(x, y);
+      const xDistance = Math.abs(source.x - target.x);
+      const yOffset = i%2 ? yOffsetScale(xDistance) : -yOffsetScale(xDistance);
+
+      return new Point(x, center.y + yOffset);
     }
   };
 
--- src/layout/tools/alignVertical.ts
+++ src/layout/tools/alignVertical.ts
@@ -1,4 +1,5 @@
-import { forceLink, forceManyBody, forceSimulation, forceX } from "d3-force";
+import { forceLink, forceManyBody, forceSimulation, forceCollide, forceX } from "d3-force";
+import { scaleLinear } from 'd3-scale'
 import { Point, Vertex } from "../";
 import { IPositioningProps } from './common';
 import getForceData from './getForceData';
@@ -8,10 +9,11 @@ const alignVertical = (props:IPositioningProps): any => {
   const { center, nodes, links, groupingLinks } = getForceData(props);
 
   const simulation = forceSimulation(nodes)
+    .force("collide", forceCollide().radius(5).strength(.01))
+    .force('links', forceLink(links).strength(.03))
     .force("x", forceX(center.x).strength(10))
-    // .force('links', forceLink(links).strength(.1))
-    .force('groupingLinks', forceLink(groupingLinks).strength(1).distance(3))
-    .force("charge", forceManyBody().strength(-1))
+    .force('groupingLinks', forceLink(groupingLinks).strength(2).distance(3))
+    .force("charge", forceManyBody().strength(-2))
     .stop()
     .tick(300)
 
@@ -22,13 +24,19 @@ const alignVertical = (props:IPositioningProps): any => {
     }
   };
 
+  const xOffsetScale = scaleLinear()
+    .domain([1, 100])
+    .range([2, 15]);
+
   const positionEdge = (e:Edge, i:number) => {
     const source = nodes.find(n => n.id === e.sourceId);
     const target = nodes.find(n => n.id === e.targetId);
     if (source && target) {
       const y = (source.y + target.y)/2;
-      const x = i%2 ? center.x + 5 : center.x - 5;
-      return new Point(x, y);
+      const yDistance = Math.abs(source.y - target.y);
+      const xOffset = i%2 ? xOffsetScale(yDistance) : -xOffsetScale(yDistance);
+
+      return new Point(center.x + xOffset, y);
     }
   };
 
