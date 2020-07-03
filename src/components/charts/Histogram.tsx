c50a93b8d637ba853a4f0ca5dc05a009c38c5d37
kjacks <kjack227@gmail.com>
2020-07-03 12:48:53 +0200

    renamed selectLeft and Right to selectStart and End

--- src/components/charts/Histogram.tsx
+++ src/components/charts/Histogram.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react'
 import { Colors } from "@blueprintjs/core";
 import {
-  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceArea, ResponsiveContainer,
+  BarChart, Bar, Cell, XAxis, Tooltip, Legend, ReferenceArea, ResponsiveContainer,
 } from 'recharts';
+import c from 'classnames';
 
-const dataFromEvent = (e: any) => (e?.activePayload[0]?.payload);
+import './Histogram.scss';
+
+const dataFromEvent = (e: any) => (e?.activePayload?.[0]?.payload);
 
 interface IHistogramDatum {
   id: string
@@ -15,12 +18,13 @@ interface IHistogramDatum {
 interface IHistogramProps {
   data: Array<IHistogramDatum>
   onSelect: (selected: any | Array<any> ) => void
+  chartProps?: any
   containerProps?: any
 }
 
 interface IHistogramState {
-  selectLeft?: IHistogramDatum
-  selectRight?: IHistogramDatum
+  selectStart?: IHistogramDatum
+  selectEnd?: IHistogramDatum
 }
 
 export class Histogram extends React.Component<IHistogramProps, IHistogramState> {
@@ -28,42 +32,60 @@ export class Histogram extends React.Component<IHistogramProps, IHistogramState>
     super(props);
 
     this.state = {};
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseUp = this.onMouseUp.bind(this);
     this.onSelect = this.onSelect.bind(this);
   }
 
+  onMouseDown(e: any) {
+    this.setState({ selectStart: dataFromEvent(e) })
+  }
+
+  onMouseMove(e: any) {
+    const { selectStart } = this.state;
+    selectStart && this.setState({ selectEnd: dataFromEvent(e) })
+  }
+
+  onMouseUp(e: any) {
+    this.onSelect(e);
+  }
+
   onSelect(e: any) {
-    const { selectLeft, selectRight } = this.state;
-    if (selectLeft && selectRight) {
-      this.props.onSelect([selectLeft.id, selectRight.id]);
+    const { selectStart, selectEnd } = this.state;
+    if (selectStart && selectEnd) {
+      this.props.onSelect([selectStart.id, selectEnd.id]);
     } else {
       this.props.onSelect(dataFromEvent(e)?.id);
     }
 
-    this.setState({ selectLeft: undefined, selectRight: undefined });
+    this.setState({ selectStart: undefined, selectEnd: undefined });
   }
 
   render() {
-    const { containerProps, data } = this.props;
-    const { selectLeft, selectRight } = this.state;
+    const { chartProps, containerProps, data } = this.props;
+    const { selectStart, selectEnd } = this.state;
 
     return (
-      <ResponsiveContainer width="100%" {...containerProps}>
-        <BarChart
-          data={data}
-          onMouseDown={e => this.setState({ selectLeft: dataFromEvent(e) })}
-          onMouseMove={e => selectLeft && this.setState({ selectRight: dataFromEvent(e) })}
-          onMouseUp={this.onSelect}
-        >
-          <XAxis dataKey="label" />
-          <YAxis />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Bar dataKey="count" fill={Colors.BLUE2} />
-          {selectLeft && selectRight && (
-            <ReferenceArea x1={selectLeft.label} x2={selectRight.label} strokeOpacity={0.3} />
-          )}
-        </BarChart>
-      </ResponsiveContainer>
+      <div className={c('Histogram', { 'dragging': selectStart != null && selectEnd != null })}>
+        <ResponsiveContainer width="100%" {...containerProps}>
+          <BarChart
+            data={data}
+            barCategoryGap="10%"
+            onMouseDown={this.onMouseDown}
+            onMouseMove={this.onMouseMove}
+            onMouseUp={this.onMouseUp}
+            {...chartProps}
+          >
+            <XAxis dataKey="label" />
+            <Tooltip offset={15} />
+            <Bar dataKey="count" fill={Colors.BLUE2} />
+            {selectStart && selectEnd && (
+              <ReferenceArea x1={selectStart.label} x2={selectEnd.label} />
+            )}
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     );
   }
 }
