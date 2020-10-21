e7359c1f32c84541fd64364b3147531af3901c03
kjacks <kjack227@gmail.com>
2020-10-21 16:44:15 +0200

    converted entity label to use latinized prop on the entity

--- src/types/Entity.tsx
+++ src/types/Entity.tsx
@@ -5,18 +5,21 @@ import c from 'classnames';
 import Schema from './Schema';
 import Transliterate from './Transliterate';
 
+interface FTMEntityExtended extends FTMEntity {
+  latinized?: any
+}
+
 interface IEntityLabelProps  {
-  entity: FTMEntity
+  entity: FTMEntityExtended
   icon?:boolean
   iconSize?: number
   truncate?:number
-  translitLookup?: any
 }
 
 class EntityLabel extends React.Component<IEntityLabelProps> {
   render() {
     const {
-      entity, icon = false, iconSize = 16, truncate, translitLookup
+      entity, icon = false, iconSize = 16, truncate
     } = this.props;
     if (!entity || !entity.id || !FTMEntity.isEntity(entity)) {
       return null;
@@ -27,7 +30,7 @@ class EntityLabel extends React.Component<IEntityLabelProps> {
     return (
       <span className={c('EntityLabel', { 'bp3-text-muted': !label })} title={caption}>
         {icon && <Schema.Icon schema={entity.schema} className="left-icon" size={iconSize} />}
-        <span><Transliterate value={label} lookup={translitLookup} /></span>
+        <span><Transliterate value={label} lookup={entity.latinized} /></span>
       </span>
     );
   }
