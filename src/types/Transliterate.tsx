d623cc7f8b8ba414e0dc58535c24003afe86a2da
kjacks <kjack227@gmail.com>
2020-10-22 14:39:07 +0200

    added clipboard functioanlity to transliterate component

--- src/types/Transliterate.tsx
+++ src/types/Transliterate.tsx
@@ -10,18 +10,58 @@ interface ITransliterateProps {
 }
 
 class Transliterate extends React.PureComponent<ITransliterateProps> {
+  constructor(props: ITransliterateProps) {
+    super(props);
+
+    this.copyText = this.copyText.bind(this);
+
+  }
+  onOpen = () => {
+    document.addEventListener('copy', this.copyText);
+  }
+
+  onClose = () => {
+    document.removeEventListener('copy', this.copyText);
+  }
+
+  copyText = (e: any) => {
+    const selection = document.getSelection();
+    e.clipboardData.setData('text/plain', this.getTranslitValue());
+    e.preventDefault();
+  }
+
+  getTranslitValue() {
+    const { lookup, value } = this.props;
+    return lookup[value];
+  }
+
   render() {
     const { lookup, value } = this.props;
-
     if (!lookup || !lookup[value]) {
       return value;
     }
+    const symbol = navigator.userAgent.indexOf('Mac OS X') != -1 ? '⌘' : 'Ctl';
+
     return (
-      <Tooltip content={lookup[value]}>
-        <span className="Transliterate bp3-tooltip-indicator">
-          {value}
-        </span>
-      </Tooltip>
+      <>
+        <Tooltip
+          popoverClassName="Transliterate__popover bp3-minimal bp3-small"
+          onOpening={this.onOpen}
+          onClosing={this.onClose}
+          content={(
+            <>
+              <span className="Transliterate__popover__main">{this.getTranslitValue()}</span>
+              <span className="Transliterate__popover__secondary">
+                <code>{symbol}</code>+<code>C</code> to copy
+              </span>
+            </>
+          )}
+        >
+          <span className="Transliterate bp3-tooltip-indicator">
+            {value}
+          </span>
+        </Tooltip>
+      </>
     );
   }
 }
