c792ae00a1c569c7d056ea8caed5d8fdc20cbb59
kjacks <kjack227@gmail.com>
2020-11-09 15:08:04 +0100

    added animated optional param to Numeric component

--- src/types/Numeric.tsx
+++ src/types/Numeric.tsx
@@ -1,28 +1,43 @@
 import React from 'react';
 import numeral from 'numeral';
+import CountUp from 'react-countup';
 import { FormattedNumber, injectIntl, WrappedComponentProps } from 'react-intl';
 
 import './Numeric.scss';
 
 interface INumericProps extends WrappedComponentProps {
-  num?:number,
+  num?:number
   abbr?:boolean
+  animate?: boolean
 }
 
 class Numeric extends React.PureComponent<INumericProps> {
+  renderAnimated(number: number) {
+    return <CountUp className="AnimatedCount__count" end={number} separator="," />
+  }
   render() {
-    const { num, abbr = false } = this.props;
+    const { num, abbr = false, animate = false } = this.props;
 
     if (num === undefined) {
       return null;
     }
-    if (!Number.isFinite(1 * num)) {
-      return <span className="Numeric">{num}</span>;
+
+    let content;
+    if (abbr) {
+      content = numeral(num).format('0a');
+      if (animate) {
+        const match = content.match(/(?<val>[0-9]+)(?<suffix>[a-z]?)/);
+        if (match?.groups) {
+          const { val, suffix } = match.groups;
+          content = <>{this.renderAnimated(+val)}{suffix}</>;
+        }
+      }
+    } else {
+      content = animate ? this.renderAnimated(num) : <FormattedNumber value={num} />
     }
+
     return (
-      <span className="Numeric">
-        {abbr ? numeral(num).format('0a') : <FormattedNumber value={num} />}
-      </span>
+      <span className="Numeric">{content}</span>
     );
   }
 }
