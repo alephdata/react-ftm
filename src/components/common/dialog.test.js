ddcfba7da657f352173414c5aaa6f6cace5c5a44
Steve Haffenden <steve.haffenden@gmail.com>
2022-01-19 11:00:46 +0100

    Rosencrantz/introduce test (#731)
    * Add jest/enzyme testing to react-ftm
    
    * Deal with some eslint issues

---
+++ src/components/common/dialog.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow, render } from 'enzyme';
+import { Dialog } from './Dialog';
+
+describe('<Dialog />', () => {
+    it('Should render correctly with no properties', () => {
+        const component = shallow(<Dialog />);
+        expect(component).toMatchSnapshot();
+    });
+
+    it('Should render correctly with properties', () => {
+        const component = shallow(<Dialog className="my-dialog" />);
+        expect(component).toMatchSnapshot();
+    });
+});
