f90d942d597f0fb3cf0706554d64595c99a509c8
Steve Haffenden <steve.haffenden@gmail.com>
2022-01-21 16:08:48 +0100

    Add another test. This required a bunch of boilerplate

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,26 @@
 // setup file
+import React from 'react';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { IntlProvider } from 'react-intl';
+import { mount, shallow } from 'enzyme';
 
 configure({ adapter: new Adapter() });
+
+const messages = require('./translations/translations.json'); // en-US.json
+const defaultLocale = 'en';
+const locale = defaultLocale;
+const filtered_messages = {};
+filtered_messages[locale] = messages[locale];
+
+global.intl = (component) => {
+    return (
+        <IntlProvider
+            locale={locale}
+            defaultLocale={defaultLocale}
+            messages={filtered_messages}
+        >
+            {React.cloneElement(component)}
+        </IntlProvider>
+    );
+}
