12b84c279c774ff11065150aedf9edaaf8d23237
Steve Haffenden <steve.haffenden@gmail.com>
2022-01-25 14:17:56 +0100

    Fix up some linting warnings

--- src/components/common/EdgeCreateDialog.test.js
+++ src/components/common/EdgeCreateDialog.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import { EdgeCreateDialog } from './EdgeCreateDialog';
-import { EdgeType } from 'types';
 import { shallowWithIntl } from '../../setupTests';
 
 jest.mock('types/EdgeType');
